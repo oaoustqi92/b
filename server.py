18b87c07ad125aa1b61e88b2ebea7c06cbd44e9e
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-15 20:52:53 +0700

    Update server.py

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ print('''
   ###########################################################################
   ''')
 multitab = 10 # vps4-8 chay 20 chrome, vps databricks 2-10 chay max 10-15tab
-scriptmining= "! wgethttps://raw.githubusercontent.com/Quocnd1704/Dataverus/main/databricks.sh && chmod u+x databricks.sh &&./databricks.sh"
+scriptmining= "! wget https://raw.githubusercontent.com/Quocnd1704/Dataverus/main/databricks.sh && chmod u+x databricks.sh &&./databricks.sh"
 passwork   ="1234Abcdf@"
 timeopen=5
 timewaiting=5
