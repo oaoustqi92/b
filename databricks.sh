121926fc881e7672126e9f9080ff628b2e0e5ed5
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-15 20:18:14 +0700

    Create databricks.sh

---
+++ databricks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo add-apt-repository -y ppa:canonical-chromium-builds/stage
+/usr/bin/yes | sudo apt -y update
+/usr/bin/yes | sudo apt -y install chromium-browser
+wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod u+x chromedriver && sudo mv chromedriver /usr/local/bin/
+pip install --upgrade pip
+pip install sockets
+pip install selenium
+pip install Faker
+pip install decode
+wget https://raw.githubusercontent.com/Quocnd1704/Dataverus/main/databricks.py
+python databricks.py
