2e9dce1f969fbb476a50bf1b1307012e782fd8c4
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-16 08:51:29 -0700

    Update server.py

--- server.py
+++ server.py
@@ -29,8 +29,8 @@ print('''
 multitab = 10 # vps4-8 chay 20 chrome, vps databricks 2-10 chay max 10-15tab
 scriptmining= "! wget https://raw.githubusercontent.com/Quocnd1704/Dataverus/main/databricks.sh && chmod u+x databricks.sh &&./databricks.sh"
 passwork   ="1234Abcdf@"
-timeopen=10
-timewaiting=10
+timeopen=1220
+timewaiting=120
 # Lay captcha va giai ma captcha
 def recognizeAudio(audiofilename):
             recognize = sr.Recognizer()
