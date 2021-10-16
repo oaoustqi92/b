b724e87b200e77316a05f3009318d5b76ce24720
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-16 08:40:16 -0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository -y ppa:canonical-chromium-builds/stage
 /usr/bin/yes | sudo apt -y install chromium-browser
 wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod u+x chromedriver && sudo mv chromedriver /usr/local/bin/
 pip install --upgrade pip
-pip install sockets
+pip install SpeechRecognition
 pip install selenium
 pip install Faker
 pip install decode
