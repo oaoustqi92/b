9d7bd9123545d5c2e4712db9e1aba86775b5f123
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-23 10:18:27 -0700

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -4,7 +4,6 @@ sudo add-apt-repository -y ppa:canonical-chromium-builds/stage
 /usr/bin/yes | sudo apt -y install chromium-browser
 wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod u+x chromedriver && sudo mv chromedriver /usr/local/bin/
 pip install --upgrade pip
-pip install SpeechRecognition
 pip install selenium
 pip install Faker
 pip install decode
